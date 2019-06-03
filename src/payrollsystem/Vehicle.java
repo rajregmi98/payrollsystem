3fdee3fe847c35d5ace3d6bc090ac29f9ab74f5a
rajregmi98 <36962803+rajregmi98@users.noreply.github.com>
2019-06-03 11:37:15 +0545

    First java project

---
+++ src/payrollsystem/Vehicle.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrollsystem;
+
+/**
+ *
+ * @author 
+ */
+public class Vehicle {
+    private String plateNumber;
+    private String color;
+    
+    public Vehicle (String plateNumber, String color) {
+        this.plateNumber = plateNumber;
+        this.color = color;
+    }
+
+    /**
+     * @return the plateNumber
+     */
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    /**
+     * @param plateNumber the plateNumber to set
+     */
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
